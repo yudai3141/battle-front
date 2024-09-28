cad1722bc7e630d28f4d4f37b66d1160ec72fabf
yudai3141 <yudaimokopipi18@icloud.com>
2024-09-28 23:27:07 +0900

    happy birthday

--- src/pages/battleResult/BattleResult.jsx
+++ src/pages/battleResult/BattleResult.jsx
@@ -5,13 +5,14 @@ import './BattleResult.css';
 
 function BattleResult() {
   const { battleId } = useParams();
-  const [result, setResult] = useState('');
+  const [result, setResult] = useState(null);
+  const PUBLIC_FOLDER = process.env.REACT_APP_PUBLIC_FOLDER;
 
   useEffect(() => {
     const fetchResult = async () => {
       try {
         const res = await axios.get(`/battles/${battleId}/process`);
-        setResult(res.data.result);
+        setResult(res.data);
       } catch (err) {
         console.error(err);
       }
@@ -19,10 +20,31 @@ function BattleResult() {
     fetchResult();
   }, [battleId]);
 
+  if (!result) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className='battleResultContainer'>
       <h2>バトル結果</h2>
-      <pre>{result}</pre>
+      {result.winnerId ? (
+        <div className='winnerSection'>
+          <p>勝者:</p>
+          <img
+            src={
+              result.winnerProfilePicture
+                ? PUBLIC_FOLDER + result.winnerProfilePicture
+                : PUBLIC_FOLDER + '/person/noAvatar.png'
+            }
+            alt={result.winnerUsername}
+            className='winnerProfileImage'
+          />
+          <p className='winnerUsername'>{result.winnerUsername}</p>
+          <p>理由: {result.reason}</p>
+        </div>
+      ) : (
+        <p>引き分けです。</p>
+      )}
     </div>
   );
 }
